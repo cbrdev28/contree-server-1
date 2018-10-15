d1f742fb48bc84742f3ca348a02fd8be7d08fcba
Charles-Basile Rouquette <cbrdev28@gmail.com>
2018-10-15 16:06:00 -0700

    Update test addPlayer

--- src/managers/__tests__/contree-game-manager.test.ts
+++ src/managers/__tests__/contree-game-manager.test.ts
@@ -22,12 +22,8 @@ test("it adds a player", () => {
 	const prevNumPlayers = manager.contreeGame.players.length;
 
 	// Action to test
-	const contreeGame = manager.addPlayer(testPlayer).contreeGame;
+	const players = manager.addPlayer(testPlayer).contreeGame.players;
 
-	// Verification
-	expect(contreeGame).not.toBeNull();
-
-	const players = contreeGame.players;
 	expect(players.length).toEqual(prevNumPlayers + 1);
 
 	const foundPlayer = players.find(player => player.id === testPlayer.id);
